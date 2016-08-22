ddfc42ac18315e99d2e8ca3305f7fc28fee2607e
ChristianBenner <christianbenner35@gmail.com>
2016-08-22 08:57:40 +0100

    Fixed logging bug, Fixed date/username interpretation issue

---
+++ src/entry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -d "cloned" ]; then
+	print "cloned EXISTS!"
+	rm -rf "cloned"
+fi
+mkdir cloned
+git clone $1 cloned
+git -C cloned/.git log --name-only --pretty=format:'>>>%ce %cd' --date=short > gitlog.log
+java -jar "gcs.jar" $2 $3
+wait 20
+sleep 20
