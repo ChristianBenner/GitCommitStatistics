2485c5dd3cf9a94c0ecde1cad28ac8a58df0249e
Christian Benner <christianbenner35@gmail.com>
2017-04-28 14:25:35 +0100

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -4,6 +4,6 @@ if [ -d "cloned" ]; then
 	rm -rf "cloned"
 fi
 mkdir cloned
-git clone $1 cloned --depth=3
+git clone $1 cloned
 git -C cloned/.git log --name-only --pretty=format:'>>>#%ce#%cd' --date=short > gitlog.log
 java -jar "gcs.jar" $2 $3 $4
